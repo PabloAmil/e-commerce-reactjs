626078aad25f750aa032a1da2cef0b42fa3e5bc2
Pablo Amil <pablo.amil91@gmail.com>
2022-09-19 22:15:18 -0300

    PreEntrega1AMIL

---
+++ src/components/CartWidget.js
@@ -0,0 +1,11 @@
+import { TbShoppingCart } from "react-icons/tb";
+
+const CartWidget = () => {
+    return (
+        <>
+        <TbShoppingCart />
+        </>
+    );
+}
+
+export default CartWidget;
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import CartWidget from "./CartWidget";
+import "./CartWidget.css";
+
+const NavBar = () => {
+    return (   
+<Navbar bg="dark" variant="dark">
+<Container>
+    <Navbar.Brand href="#home">
+    BlackAndWine
+    
+    </Navbar.Brand>
+    <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#productos">Productos</Nav.Link>
+        <Nav.Link href="#contacto">Contacto</Nav.Link>
+    </Nav>
+    <CartWidget />
+</Container>
+</Navbar>
+    
+    )
+}
+
+export default NavBar;
+
