5e3f92611023a26a9e16541d53eaa44f26fd907f
Pablo Amil <pablo.amil91@gmail.com>
2022-10-05 03:36:22 -0300

    PreEntrega2AMIL

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,29 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import CartWidget from "./CartWidget";
-import "./CartWidget.css";
+import { Link } from "react-router-dom";
+import CartWidget from "./CartWidget/CartWidget";
+
 
 const NavBar = () => {
     return (   
-<Navbar bg="dark" variant="dark">
-<Container>
-    <Navbar.Brand href="#home">
-    BlackAndWine
-    
-    </Navbar.Brand>
-    <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#productos">Productos</Nav.Link>
-        <Nav.Link href="#contacto">Contacto</Nav.Link>
-    </Nav>
-    <CartWidget />
-</Container>
-</Navbar>
-    
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="#home">
+                <Link to={'/'}>
+                BlackAndWine
+                </Link>
+            </Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                <Nav.Link as={Link} to='/category/productos'>Productos</Nav.Link>
+                <Nav.Link as={Link} to='/category/Blancos'>Blancos</Nav.Link>
+                <Nav.Link as={Link} to='/category/Tintos'>Tintos</Nav.Link>
+            </Nav>
+            <CartWidget />
+        </Container>
+    </Navbar>
     )
 }
+
 
 export default NavBar;
 
