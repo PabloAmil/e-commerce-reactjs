664233cafc810a91406f2f8094c1851eaf1403ba
Pablo Amil <pablo.amil91@gmail.com>
2022-10-24 18:21:44 -0300

    deploy settings

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
     return (
         <>
-        <BrowserRouter>
+        <BrowserRouter basename='/e-commerce-reactjs'>
             <CartProvider>
                 <NavBar />
                 <Routes>
